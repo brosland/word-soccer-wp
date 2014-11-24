8be52224330248fafdefe994c9d2baa4ed70176e
unknown <mbskot@gmail.com>
2014-11-25 00:01:32 +0100

    Init

---
+++ WordSoccer/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace WordSoccer
+{
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+
+			this.NavigationCacheMode = NavigationCacheMode.Required;
+
+			DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+			StatusBar sb = Windows.UI.ViewManagement.StatusBar.GetForCurrentView();
+
+			sb.HideAsync();
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			// TODO: Prepare page for display here.
+
+			// TODO: If your application contains multiple pages, ensure that you are
+			// handling the hardware Back button by registering for the
+			// Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+			// If you are using the NavigationHelper provided by some templates,
+			// this event is handled for you.
+		}
+
+	}
+}
