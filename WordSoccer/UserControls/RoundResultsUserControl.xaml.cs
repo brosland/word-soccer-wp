f38932919654d905210a9d19c18019cb7ae0b583
unknown <mbskot@gmail.com>
2014-11-27 21:24:01 +0100

    Added layout for round and final results.

---
+++ WordSoccer/UserControls/RoundResultsUserControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WordSoccer.UserControls
+{
+	public sealed partial class RoundResultsUserControl : UserControl
+	{
+		public RoundResultsUserControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ WordSoccer/UserControls/RoundUserControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WordSoccer.UserControls
+{
+	public sealed partial class RoundUserControl : UserControl
+	{
+		public RoundUserControl()
+		{
+			InitializeComponent();
+
+			InitializeSelectedButtons();
+
+			InitializeInputButtons();
+		}
+
+		private void InitializeInputButtons()
+		{
+			for (int i = 0; i < 11; i++)
+			{
+				Button button = new Button();
+				button.Style = (Style) Application.Current.Resources["LetterButtonStyle"];
+				button.Content = (char) ('A' + i);
+				
+				Grid.SetColumn(button, i);
+				InputLettersGrid.Children.Add(button);
+			}
+		}
+
+		private void InitializeSelectedButtons()
+		{
+			for (int i = 0; i < 11; i++)
+			{
+				Button button = new Button();
+				button.Style = (Style) Application.Current.Resources["LetterButtonStyle"];
+				button.Content = (char) ('A' + i);
+
+				Grid.SetColumn(button, i);
+				SelecteLettersGrid.Children.Add(button);
+			}
+
+			Button submitButton = new Button();
+			submitButton.Style = (Style) Application.Current.Resources["SubmitButtonStyle"];
+			submitButton.Content = "Submit";
+
+			Grid.SetColumn(submitButton, 11);
+			SelecteLettersGrid.Children.Add(submitButton);
+		}
+	}
+}
